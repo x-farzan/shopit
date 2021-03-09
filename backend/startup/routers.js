c5d9b059fa83a8fbdfaf81023baa14b0ac5c18ff
ehmusman <ehmusman@gmail.com>
2021-03-09 11:50:22 +0500

    password is required error is solved

--- backend/startup/routers.js
+++ backend/startup/routers.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const products = require('../routes/product')
 const error = require('../middleware/errors')
+const users = require('../routes/users')
+const auth = require('../routes/auth')
 
 module.exports = function (app) {
     app.use(express.json())
     app.use('/api/v1', products)
-
+    app.use('/api/v1', users)
+    app.use('/api/v1', auth)
 
 
     // error middleware
