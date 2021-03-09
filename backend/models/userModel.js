281e6e8f6c61626b6de2d535a5bd59ab4897f025
ehmusman <ehmusman@gmail.com>
2021-03-09 11:49:24 +0500

    usermodel is created

---
+++ backend/models/userModel.js
@@ -0,0 +1,82 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const PasswordComplexity = require('joi-password-complexity');
+const jwt = require('jsonwebtoken');
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        minlength: 5,
+        maxlength: 50,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        minlength: 5,
+        maxlength: 50,
+        required: true
+    },
+    password: {
+        type: String,
+        minlength: 8,
+        maxlength: 1024,
+        required: true
+    },
+    avatar: {
+        public_id: {
+            type: String,
+            // required: true
+        },
+        url: {
+            type: String,
+            // required: true
+        }
+    },
+    role: {
+        type: String,
+        default: 'user'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date
+})
+
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY)
+    return token
+}
+const User = mongoose.model("User", userSchema)
+
+const userValidator = (user) => {
+
+    const schema = Joi.object({
+        name: Joi.string().min(3).required(),
+        email: Joi.string().min(5).required().email(),
+        password: new PasswordComplexity({
+            min: 8,
+            max: 50,
+            lowerCase: 1,
+            upperCase: 1,
+            numeric: 1,
+            symbol: 1
+        }),
+        avatar: Joi.object({
+            public_id: Joi.string(),
+            url: Joi.string()
+        }),
+        role: Joi.string(),
+        resetPasswordToken: Joi.string()
+    })
+    return schema.validate(user)
+
+}
+
+
+
+exports.User = User;
+exports.validation = userValidator;
