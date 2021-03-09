4d3723a1fb3912593c7033a3f329ad8285e94af3
ehmusman <ehmusman@gmail.com>
2021-03-09 14:13:34 +0500

    auth is implemented for login users

---
+++ backend/middleware/auth.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+function auth(req, res, next) {
+    const token = req.header('x-auth-token')
+    if (!token) return res.status(401).send("Access Denied: No Token Provided")
+    try {
+        const decoded = jwt.verify(token, process.env.JWTPRIVATEKEY)
+        req.user = decoded;
+        next()
+    } catch (ex) {
+        res.status(400).send("Invalid Token!")
+    }
+}
+module.exports = auth;
