5b8b39c172e3ada0e2574a93af6f761faa2b9de2
ehmusman <ehmusman@gmail.com>
2021-03-11 14:46:49 +0500

    order model and schema is created

---
+++ backend/models/orderModel.js
@@ -0,0 +1,74 @@
+const mongoose = require('mongoose');
+
+
+const orderSchema = new mongoose.Schema({
+    shippingInfo: {
+        address: { type: String, required: true },
+        city: { type: String, required: true },
+        phoneNo: { type: String, required: true },
+        postalCode: { type: String, required: true },
+        country: { type, String, required: true }
+
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    orderItems: [
+        {
+            name: { type: String, required: true },
+            quantity: { type: String, required: true },
+            image: { type: String, required: true },
+            price: { type: String, required: true },
+            Product: {
+                type: mongoose.Schema.Types.ObjectId,
+                required: true,
+                ref: 'Product'
+            }
+        }
+    ],
+    paymentInfo: {
+        id: { type: String },
+        status: { type: String }
+    },
+    paidAt: {
+        type: Date
+    },
+    itemPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    taxPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    shippingPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    totalPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    orderStatus: {
+        type: String,
+        required: true,
+        default: "processing..."
+    },
+    deliveredAt: { type: String },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Order = mongoose.model('Order', orderSchema)
+
+
+
+module.exports = Order
