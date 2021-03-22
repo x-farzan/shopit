dc8d037d5c68f392cc65e7b332a2c2424884df78
ehmusman <ehmusman@gmail.com>
2021-03-22 14:36:13 +0500

    rating component is created

---
+++ frontend/src/components/layout/Rating.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export const Rating = ({ rating, reviews, price, appr }) => {
+    const parent = {
+        position: "relative",
+        width: "90px",
+        height: "20px",
+    }
+    const child = {
+        position: "absolute",
+        top: "0",
+        right: "0",
+        width: `${100 - ((rating / 5) * 100)}%`,
+        height: "100%",
+        backgroundColor: "#fff"
+    }
+    return (
+        <>
+            <div className="my-3 text-warning d-inline-block" style={parent} >
+                <div style={child}></div>
+                <i className="fa fa-star"></i>
+                <i className="fa fa-star"></i>
+                <i className="fa fa-star"></i>
+                <i className="fa fa-star"></i>
+                <i className="fa fa-star"></i>
+            </div>
+            <span className='text-dark mx-1'>({reviews} reviews)</span>
+            <p className={`price ${appr}`}>$ {price}</p>
+        </>
+    )
+}
