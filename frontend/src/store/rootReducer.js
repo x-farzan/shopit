8454672f99f2dfe44516231731a8898b04a7b083
ehmusman <ehmusman@gmail.com>
2021-03-23 12:55:06 +0500

    log file data is removed

--- frontend/src/store/rootReducer.js
+++ frontend/src/store/rootReducer.js
@@ -2,10 +2,8 @@ import { combineReducers } from '@reduxjs/toolkit';
 
 import productsReducer from './products'
 import productDetailReducer from './productDetails'
-import metaDataReducer from './metaData'
 
 export default combineReducers({
     products: productsReducer,
     productDetail: productDetailReducer,
-    metaData: metaDataReducer
 })
