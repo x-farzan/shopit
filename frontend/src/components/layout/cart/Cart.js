473654ce2b12a710a8c79b750af1627736cc8f8c
ehmusman <ehmusman@gmail.com>
2021-03-29 15:09:53 +0500

    cart component is created

---
+++ frontend/src/components/layout/cart/Cart.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Metadata from '../products/Metadata'
+
+const Cart = () => {
+    return (
+        <>
+            <Metadata title="Your Cart" />
+
+        </>
+    )
+}
+
+export default Cart
