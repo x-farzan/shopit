7b67791870e1afe8c0de8e1b8e23ace386b51c63
ehmusman <ehmusman@gmail.com>
2021-03-31 11:55:30 +0500

    /api/v1/user/update route is created to update the user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { User, validation } = require('../models/userModel')
+const { User, validation, updateUserValidation } = require('../models/userModel')
 const _ = require('lodash')
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
@@ -19,7 +19,6 @@ router.get('/me', auth, async (req, res) => {
 
 // register a user
 router.post('/users', async (req, res) => {
-    // console.log("backend", req.body)
 
     // const { avatar } = req.files
     const { name, email, password, avatar } = req.body
@@ -57,6 +56,68 @@ router.post('/users', async (req, res) => {
 
     sendToken(user, 200, res)
 
+})
+
+// update the existing  user
+// protecting by user
+router.post('/user/update', auth, async (req, res) => {
+    let user = await User.findById(req.user._id)
+
+    const { name, email, avatar } = req.body
+    if (avatar === '') return res.status(400).send("No File Uploaded")
+
+    // joi data validation
+    let userData = {
+        name,
+        email,
+        avatar: {
+            public_id: user.avatar.public_id,
+            url: user.avatar.url
+        }
+    }
+    // joi validation for error
+    const { error } = updateUserValidation(userData)
+    if (error) return res.status(400).send(error.details[0].message)
+
+    // check if user changed the email
+    if (user.email !== email) {
+        // check if user is already exists
+        let existingUser = await User.findOne({ email })
+        if (existingUser) return res.status(400).send("This Email is already registered by a different User, Use another email")
+    }
+
+
+    // delete the picture from cloudinary
+    await cloudinary.v2.uploader.destroy(user.avatar.public_id)
+    // upload new picture to cloudinary
+    const result = await cloudinary.v2.uploader.upload(avatar, {
+        folder: "avatar",
+        width: 150,
+        crop: 'scale'
+    })
+    const { public_id, secure_url } = result
+
+    // update userData
+    userData = {
+        name,
+        email,
+        avatar: {
+            public_id,
+            url: secure_url
+        }
+    }
+
+    // check if the user does not changed the email
+    if (user.email === email) {
+        user = await User.findByIdAndUpdate(user._id, {
+            $set: userData
+
+        }, { new: true, runValidators: true, useFindAndModify: false })
+    }
+
+    await user.save();
+
+    sendToken(user, 200, res)
 })
 
 module.exports = router
