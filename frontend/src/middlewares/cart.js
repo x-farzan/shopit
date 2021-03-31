9fa1700b178130e0205b78045d97e01b7e63d876
ehmusman <ehmusman@gmail.com>
2021-04-01 00:04:30 +0500

    cart middleware is created and registered. so that we can add cartItems in the local storage

---
+++ frontend/src/middlewares/cart.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+import { cartCallBegan, cartCallFailed, cartCallSuccess } from '../store/api'
+
+const cart = ({ dispatch, getState }) => next => async action => {
+    if (action.type !== cartCallBegan.type) return next(action)
+
+    // destructuring the action.payload
+    const { url, onStart, onSuccess, onError, qty, id } = action.payload
+
+    if (onStart) dispatch({ type: onStart })
+
+    next(action)
+
+    try {
+        const response = await axios.request({
+            url
+        })
+        dispatch(cartCallSuccess(response.data))
+        if (onSuccess) dispatch({ type: onSuccess, payload: response.data, qty, id })
+        // console.log(getState().entities.cart.list)
+        localStorage.setItem("cartItems", JSON.stringify(getState().entities.cart.list))
+    } catch (error) {
+        dispatch(cartCallFailed(error.message))
+        if (onError) dispatch({ type: onError, payload: error.message })
+    }
+}
+export default cart
