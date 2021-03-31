f13e7967599d3bf57e4030be144d262137090b74
ehmusman <ehmusman@gmail.com>
2021-04-01 00:05:00 +0500

    cart reducer is added

--- frontend/src/store/rootReducer.js
+++ frontend/src/store/rootReducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from '@reduxjs/toolkit';
 
 import productsReducer from './products'
 import productDetailReducer from './productDetails'
+import cartReducer from './cart'
 
 export default combineReducers({
     products: productsReducer,
     productDetail: productDetailReducer,
+    cart: cartReducer
 })
