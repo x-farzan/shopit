6ee019afc361270cadd9a3802eba8362ccd0a5d4
ehmusman <ehmusman@gmail.com>
2021-04-01 12:56:22 +0500

    Cart component is broken down to two components

--- frontend/src/components/layout/cart/Cart.js
+++ frontend/src/components/layout/cart/Cart.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Metadata from '../products/Metadata'
+import { useSelector } from 'react-redux'
+import CartItem from './CartItem'
+import Summary from './Summary'
 
 const Cart = () => {
+    const { list } = useSelector(state => state.entities.cart)
+    const itemsPrice = list.map(i => i.totalPrice)
+    const totalPrice = itemsPrice.reduce((a, b) => a + b, 0)
+
     return (
         <>
             <Metadata title="Your Cart" />
+            <div className="container" style={{ minHeight: "100vh" }}>
+                <div className="row">
+                    <div className="col-md-8">
+                        {list.map(item => (
+                            <CartItem
+                                key={item._id}
+                                name={item.name}
+                                price={item.price}
+                                stock={item.stock}
+                                qty={item.qty}
+                                id={item._id}
+                            />
+                        ))}
+                    </div>
+                    <div className="col-md-4" >
+                        <Summary
+                            length={list.length}
+                            totalPrice={totalPrice}
+                        />
+                    </div>
+                </div>
+
+            </div>
 
         </>
     )
