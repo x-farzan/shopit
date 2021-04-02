ebafe354a180bbd0eeabadccf31183016f75bf66
ehmusman <ehmusman@gmail.com>
2021-04-02 12:32:45 +0500

    Final Summary component is created

---
+++ frontend/src/components/layout/cart/FinalSummary.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+
+
+const FinalSummary = ({ subTotal, shipping, totalPrice, tax }) => {
+    const history = useHistory()
+    const handleOnClick = () => {
+        const data = {
+            itemsPrice: subTotal,
+            shipping,
+            tax,
+            totalPrice
+        }
+        sessionStorage.setItem("FinalOrderInfo", JSON.stringify(data))
+        history.push("/")
+    }
+    return (
+        <>
+            <div className="card">
+                <div className="card-header">
+                    Order Summary
+                            </div>
+                <div className="card-body">
+                    <hr />
+                    <p className="d-flex justify-content-between">
+                        <span>Subtotal:  </span>
+                        <span>${subTotal}</span>
+                    </p>
+                    <hr />
+                    <p className="d-flex justify-content-between">
+                        <span>Shipping:  </span>
+                        <span>${shipping}</span>
+                    </p>
+                    <hr />
+
+                    <p className="d-flex justify-content-between">
+                        <span>Tax: </span>
+                        <span>${tax}</span>
+                    </p>
+                    <hr />
+                    <p className="d-flex justify-content-between">
+                        <span>Est. total Price:</span>
+                        <span>  $ {totalPrice}</span>
+                    </p>
+                    <hr />
+                </div>
+                <div className="card-footer">
+                    <button
+                        onClick={handleOnClick}
+                        className="btn btn-warning btn-block">
+                        Proceed To Payment
+                    </button>
+                </div>
+            </div>
+        </>
+
+
+    )
+}
+
+export default FinalSummary
