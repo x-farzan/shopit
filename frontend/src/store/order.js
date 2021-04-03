e62d13b91495c8d87c579f24f4e775dfa7b99790
ehmusman <ehmusman@gmail.com>
2021-04-03 14:19:35 +0500

    order reducer component is created

---
+++ frontend/src/store/order.js
@@ -0,0 +1,50 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { apiCallBegan } from './api'
+
+/////////////////////////////////////////////////////////////////////
+//                          Reducer
+/////////////////////////////////////////////////////////////////////
+const slice = createSlice({
+    name: "order",
+    initialState: {
+        loading: false,
+        error: null,
+        ordered: {}
+    },
+    reducers: {
+        createOrderRequest: (order, action) => {
+            order.loading = true
+        },
+        createOrderSuccess: (order, action) => {
+            order.loading = false
+            order.ordered = action.payload
+        },
+        ctreateOrderFailed: (order, action) => {
+            order.loading = false;
+            order.error = action.payload
+        }
+    }
+})
+
+export default slice.reducer
+
+
+//////////////////////////////////////////////////////////////////////
+//                      Actions
+//////////////////////////////////////////////////////////////////////
+const { createOrderRequest, createOrderSuccess, ctreateOrderFailed } = slice.actions
+
+export const creatingOrderRequest = (data) => (dispatch) => {
+    console.log(data)
+    dispatch(
+        apiCallBegan({
+            url: "/api/v1/order/new",
+            method: 'post',
+            data,
+            headers: { "Content-Type": 'application/json' },
+            onStart: createOrderRequest.type,
+            onSuccess: createOrderSuccess.type,
+            onError: ctreateOrderFailed.type
+        })
+    )
+}
