95cac7b2015f2dad4d59756fa3191f271797d995
ehmusman <ehmusman@gmail.com>
2021-04-03 15:20:10 +0500

    a littlebit css animation is added

--- frontend/src/components/layout/cart/PaymentSuccess.js
+++ frontend/src/components/layout/cart/PaymentSuccess.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from "react-router-dom"
+import useWebAnimations from "@wellyshen/use-web-animations";
+
+
 const PaymentSuccess = () => {
-    console.log("hello")
+
     return (
         <div className="container pt-5" style={{ minHeight: "100vh" }}>
-            <div className="d-flex flex-column align-items-center justify-content-center">
-                <i className="fas fa-10x  text-success my-auto fa-check-circle"></i>
+            <div className="animate__animated animate__backInDown d-flex target flex-column align-items-center justify-content-center">
+                <i style={{ fontSize: "20rem" }} className="fas text-success my-auto fa-check-circle"></i>
                 <h3>Your Order Has Been Placed Successfully</h3>
-                <h3>Go to <Link to='/orders'>Your Orders</Link></h3>
+                <h3>Go to <br /> <Link style={{ fontSize: "20px" }} to='/orders'>Your Orders</Link></h3>
             </div>
         </div>
     )
--- frontend/src/components/layout/products/Product.js
+++ frontend/src/components/layout/products/Product.js
@@ -6,7 +6,7 @@ const Product = ({ id, price, reviews, wide, name, rating }) => {
 
     return (
         <div className={`${wide} my-3`}>
-            <div className="card">
+            <div className="card animate__animated animate__headShake">
                 <img className="card-img-top" src="/airpods3.jpg" alt="" />
                 <div className="card-body">
                     <Link to={`/product/${id}`} style={{ textDecoration: 'none' }} className="card-text d-block text-dark">{name}</Link>
