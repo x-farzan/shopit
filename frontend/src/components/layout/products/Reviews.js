e291789bd74f0f3aa8f2bbe007aa1d9926207242
ehmusman <ehmusman@gmail.com>
2021-04-05 11:24:16 +0500

    Reviews component is created to show all the reviews

---
+++ frontend/src/components/layout/products/Reviews.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Reviews = () => {
+    return (
+        <div className="container my-5">
+            <h1>Other's Reviews</h1>
+            <div className="row">
+                <div className="col-md-6">
+                    <hr />
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Reviews
