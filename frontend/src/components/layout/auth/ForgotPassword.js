c4342297470b449f67342dd912fb3ec691e76c5a
ehmusman <ehmusman@gmail.com>
2021-04-08 16:13:14 +0500

    ForgotPassword componant is just created

---
+++ frontend/src/components/layout/auth/ForgotPassword.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Metadata from '../products/Metadata'
+
+const ForgotPassword = () => {
+    return (
+        <div>
+            <Metadata title="Forgot Password" />
+
+        </div>
+    )
+}
+
+export default ForgotPassword
