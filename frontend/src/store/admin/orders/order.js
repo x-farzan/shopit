4ca8106c527d03930bca11a3e9a78e0bb4a232a0
ehmusman <ehmusman@gmail.com>
2021-04-09 23:17:27 +0500

    for now i have learnt that keep the state at the same place with action but in isolation. means at same place with isolation

--- frontend/src/store/admin/orders/order.js
+++ frontend/src/store/admin/orders/order.js
@@ -1,11 +1,190 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import getAllOrdersReducer from "./getAllOrders"
-import deleteOrderReducer from "./deleteOrder"
-import getSingleOrderReducer from './getSingleOrder'
-import changeOrderStatus from "./changeOrderStatus"
-export default combineReducers({
-    getOrders: getAllOrdersReducer,
-    deleteOrder: deleteOrderReducer,
-    singleOrder: getSingleOrderReducer,
-    orderStatus: changeOrderStatus
+import { createSlice } from "@reduxjs/toolkit"
+import { apiCallBegan } from "../../api"
+
+
+////////////////////////////////////////////////////////////////////
+//                              Reducers
+////////////////////////////////////////////////////////////////////
+
+const slice = createSlice({
+    name: "admin",
+    initialState: {
+
+        // get all orders
+        gAOLoading: false,
+        gAOError: null,
+        orders: [],
+
+
+        // get single order
+        gSOLoading: false,
+        gSOError: null,
+        order: {},
+
+
+        // change order status
+        cOLoading: false,
+        cOError: null,
+        statusOfOrder: ""
+    },
+    reducers: {
+        getAllOrdersRequest: (admin, action) => {
+            admin.gAOLoading = true
+            admin.gAOError = null
+        },
+        getAllOrdersSuccess: (admin, action) => {
+            admin.gAOLoading = false
+            admin.orders = action.payload.orders
+        },
+        getAllOrdersFailed: (admin, action) => {
+            admin.gAOLoading = false
+            admin.gAOError = action.payload
+        },
+        resettingGetAllOrders: (admin, action) => {
+            admin.gAOLoading = false
+            admin.gAOError = null
+            admin.orders = []
+        },
+
+
+        getSingleOrderRequest: (admin, action) => {
+            admin.gSOLoading = true
+            admin.gSOError = null
+        },
+        getSingleOrderSuccess: (admin, action) => {
+            admin.gSOLoading = false
+            admin.order = action.payload
+        },
+
+        getSingleOrderFailed: (admin, action) => {
+            admin.gSOLoading = false
+            admin.gSOError = action.payload
+        },
+        resettingGetSingleOrder: (admin, action) => {
+            admin.gSOLoading = false
+            admin.gSOError = null
+            admin.order = {}
+        },
+
+
+        changeOrderStatusRequest: (admin, action) => {
+            admin.cOLoading = true
+            admin.cOError = null
+        },
+        changeOrderStatusSuccess: (admin, action) => {
+            admin.cOLoading = false
+            admin.statusOfOrder = action.payload
+        },
+        changeOrderStatusFailed: (admin, action) => {
+            admin.cOLoading = false
+            admin.cOError = action.payload
+        },
+        resettingChangeOrderStatus: (admin, action) => {
+            admin.cOLoading = false
+            admin.cOError = null
+            admin.statusOfOrder = ""
+        },
+
+
+        deleteOrderRequest: (admin, action) => {
+            admin.dOLoading = true
+            admin.dOError = null
+        },
+        deleteOrderSuccess: (admin, action) => {
+            admin.dOLoading = false
+            admin.isDeleted = action.payload.success
+        },
+        deleteOrderFailed: (admin, action) => {
+            admin.dOLoading = false
+            admin.dOError = null
+        },
+        resettingDeleteOrder: (admin, action) => {
+            admin.dOLoading = false
+            admin.dOError = null
+            admin.isDeleted = false
+        }
+    },
 })
+
+
+export default slice.reducer
+////////////////////////////////////////////////////////////////////////////
+//                                      actions
+////////////////////////////////////////////////////////////////////////////
+
+const {
+    getAllOrdersFailed,
+    getAllOrdersRequest,
+    getAllOrdersSuccess,
+    resettingGetAllOrders,
+    getSingleOrderFailed,
+    getSingleOrderRequest,
+    getSingleOrderSuccess,
+    resettingGetSingleOrder,
+    changeOrderStatusFailed,
+    changeOrderStatusRequest,
+    changeOrderStatusSuccess,
+    resettingChangeOrderStatus,
+    deleteOrderFailed,
+    deleteOrderRequest,
+    deleteOrderSuccess,
+    resettingDeleteOrder
+} = slice.actions
+
+export const gettingAllOrders = () => dispatch => {
+    dispatch(
+        apiCallBegan({
+            url: `/api/v1/admin/orders`,
+            method: "get",
+            onStart: getAllOrdersRequest.type,
+            onSuccess: getAllOrdersSuccess.type,
+            onError: getAllOrdersFailed.type
+        })
+    )
+}
+export const resetGetOrdersError = () => resettingGetAllOrders()
+
+
+export const gettingSingleOrderRequest = (id) => dispatch => {
+    dispatch(
+        apiCallBegan({
+            url: `/api/v1/admin/order/${id}`,
+            method: "get",
+            onStart: getSingleOrderRequest.type,
+            onSuccess: getSingleOrderSuccess.type,
+            onError: getSingleOrderFailed.type
+        })
+    )
+}
+export const resetGetSingleOrder = () => resettingGetSingleOrder()
+
+
+export const changingOrderStatusRequest = (id, data) => dispatch => {
+    dispatch(
+        apiCallBegan({
+            url: `/api/v1/admin/update/order/${id}`,
+            method: "put",
+            data,
+            headers: { "Content-Type": 'application/json' },
+            onStart: changeOrderStatusRequest.type,
+            onSuccess: changeOrderStatusSuccess.type,
+            onError: changeOrderStatusFailed.type
+        })
+    )
+}
+export const resetChangeOrderStatus = () => resettingChangeOrderStatus()
+
+
+export const deletingOrderRequest = (id) => dispatch => {
+
+    dispatch(
+        apiCallBegan({
+            url: `/api/v1/admin/delete/order/${id}`,
+            method: 'delete',
+            onStart: deleteOrderRequest.type,
+            onSuccess: deleteOrderSuccess.type,
+            onError: deleteOrderFailed.type
+        })
+    )
+}
+export const resetDeleteOrderError = () => resettingDeleteOrder()
--- frontend/src/store/admin/users/users.js
+++ frontend/src/store/admin/users/users.js
@@ -1,11 +1,188 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import getAllUsersReducer from "./getAllUsers"
-import deleteUserReducer from './deleteUser'
-import getSingleUserReducer from "./getSingleUser"
-import updateUserReducer from "./updateUser"
-export default combineReducers({
-    getUsers: getAllUsersReducer,
-    deleteUser: deleteUserReducer,
-    singleUser: getSingleUserReducer,
-    updateUser: updateUserReducer
+import { createSlice } from "@reduxjs/toolkit"
+import { apiCallBegan } from "../../api"
+
+
+////////////////////////////////////////////////////////////////////
+//                              Reducers
+////////////////////////////////////////////////////////////////////
+
+const slice = createSlice({
+    name: "admin",
+    initialState: {
+        // update user
+        uULoading: false,
+        uUError: null,
+        isUserUpdated: false,
+        // get single user
+        gSULoading: false,
+        gSUError: null,
+        user: {},
+
+        // Get All Users
+        gAULoading: false,
+        gAUError: null,
+        users: [],
+
+        // delete User
+        dULoading: false,
+        dUError: null,
+        isDeleted: false
+    },
+    reducers: {
+        updateUserRequest: (admin, action) => {
+            admin.uULoading = true
+            admin.uUError = action.payload
+        },
+        updateUserSuccess: (admin, action) => {
+            admin.uULoading = false
+            admin.isUserUpdated = action.payload.success
+        },
+        updateUserFailed: (admin, action) => {
+            admin.uULoading = false
+            admin.uUError = action.payload
+        },
+        resettingUpdateUser: (admin, action) => {
+            admin.uULoading = false
+            admin.uUError = null
+            admin.isUserUpdated = false
+        },
+
+
+        getSingleUserRequest: (admin, action) => {
+            admin.gSULoading = true
+            admin.gSUError = null
+        },
+        getSingleUserSuccess: (admin, action) => {
+            admin.gSULoading = false
+            admin.user = action.payload
+        },
+        getSingleUserFailed: (admin, action) => {
+            admin.gSULoading = false
+            admin.gSUError = action.payload
+        },
+        resettingGetSingleUser: (admin, action) => {
+            admin.gSULoading = false
+            admin.gSUError = null
+            admin.user = {}
+        },
+
+        getAllUsersRequest: (admin, action) => {
+            admin.gAULoading = true;
+            admin.gAUError = null
+        },
+        getAllUsersSuccess: (admin, action) => {
+            admin.gAULoading = false
+            admin.users = action.payload
+        },
+        getAllUsersFailed: (admin, action) => {
+            admin.gAULoading = false
+            admin.gAUError = action.payload
+        },
+        resettingGetAllUsers: (admin, action) => {
+            admin.gAULoading = false
+            admin.gAUError = null
+            admin.users = []
+        },
+
+        deleteUserRequest: (admin, action) => {
+            admin.dULoading = true
+            admin.dUError = null
+        },
+        deleteUserSuccess: (admin, action) => {
+            admin.dULoading = false
+            admin.isDeleted = action.payload.success
+        },
+        deleteUserFailed: (admin, action) => {
+            admin.dULoading = false
+            admin.dUError = null
+        },
+        resettingDeleteUser: (admin, action) => {
+            admin.dULoading = false
+            admin.dUError = null
+            admin.isDeleted = false
+        }
+    },
 })
+
+
+export default slice.reducer
+////////////////////////////////////////////////////////////////////////////
+//                                      actions
+////////////////////////////////////////////////////////////////////////////
+
+const {
+    updateUserFailed,
+    updateUserRequest,
+    updateUserSuccess,
+    resettingUpdateUser,
+    getAllUsersFailed,
+    getAllUsersRequest,
+    getAllUsersSuccess,
+    resettingGetAllUsers,
+    getSingleUserFailed,
+    getSingleUserRequest,
+    getSingleUserSuccess,
+    resettingGetSingleUser,
+    deleteUserFailed,
+    deleteUserRequest,
+    deleteUserSuccess,
+    resettingDeleteUser
+} = slice.actions
+
+
+export const updatingUserRequest = (data, id) => dispatch => {
+    dispatch(
+        apiCallBegan({
+            url: `/api/v1/admin/user/${id}`,
+            method: "put",
+            data,
+            headers: { "Content-Type": 'multipart/form-data' },
+            onStart: updateUserRequest.type,
+            onSuccess: updateUserSuccess.type,
+            onError: updateUserFailed.type
+        })
+    )
+}
+
+export const resetUpdateUser = () => resettingUpdateUser()
+
+
+export const gettingSingleUserRequest = (id) => dispatch => {
+    dispatch(
+        apiCallBegan({
+            url: `/api/v1/admin/user/${id}`,
+            method: "get",
+            onStart: getSingleUserRequest.type,
+            onSuccess: getSingleUserSuccess.type,
+            onError: getSingleUserFailed.type
+        })
+    )
+}
+
+export const resetGetSingleUser = () => resettingGetSingleUser()
+
+export const gettingAllUsersRequest = () => dispatch => {
+    dispatch(
+        apiCallBegan({
+            url: "/api/v1/admin/all/users",
+            method: "get",
+            onStart: getAllUsersRequest.type,
+            onSuccess: getAllUsersSuccess.type,
+            onError: getAllUsersFailed.type
+        })
+    )
+}
+export const resetGetAllUsers = () => resettingGetAllUsers()
+
+export const deletingUserRequest = (id) => dispatch => {
+    dispatch(
+        apiCallBegan({
+            url: `/api/v1/admin/user/${id}`,
+            method: "delete",
+            onStart: deleteUserRequest.type,
+            onSuccess: deleteUserSuccess.type,
+            onError: deleteUserFailed.type
+        })
+    )
+}
+export const resetDeleteUser = () => resettingDeleteUser()
