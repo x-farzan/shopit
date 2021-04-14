3fc4ba50816bd0d5215ea607bbf1fc657ef4e008
ehmusman <ehmusman@gmail.com>
2021-04-15 02:31:39 +0500

    /frontend/build/index.html path is set

--- backend/server.js
+++ backend/server.js
@@ -32,9 +32,9 @@ require('./startup/logging')();
 
 if (process.env.NODE_ENV === 'production') {
     console.log("production mode")
-    app.use(express.static(path.join(__dirname, "../frontend/build")))
+    app.use(express.static(path.join(__dirname, "/frontend/build")))
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
     })
 } else {
     app.get("/", (req, res) => {
