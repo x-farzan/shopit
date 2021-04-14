944de4d18f1501e3bdfbcdcbc8d9a9433fbcdb6d
ehmusman <ehmusman@gmail.com>
2021-04-15 03:20:37 +0500

    path changed

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ require('./startup/logging')();
 
 if (process.env.NODE_ENV === 'production') {
     console.log("production mode")
-    app.use(express.static(path.join(__dirname, "/frontend/build")))
+    app.use(express.static(path.join(__dirname, "frontend/build")))
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
     })
